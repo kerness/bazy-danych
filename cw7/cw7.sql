e45454f4691f2afb3a5eab25038f1a90cd4e2963
Maciej Bąk <maciej.bak260@gmail.com>
2021-05-08 14:54:58 +0200

    cw7

---
+++ cw7/cw7.sql
@@ -0,0 +1,33 @@
+create or replace function FibonacciSequence(upTo int) returns void as $$
+declare 
+	num1 integer := 0;
+	num2 integer := 1;
+	numCount integer := 0;
+	numNext integer := 0;
+
+begin
+	raise notice 'Fibonacci sequence up to % numbers', upTo;
+	if upTo = 0 then
+		raise notice 'Enter number higher than 0.';
+	elsif upTo = 1 then
+		raise notice '%', num1;
+	else
+		while numCount < upTo loop
+			raise notice '%', num1;
+			numNext := num1 + num2;
+			num1 := num2;
+			num2 := numNext;
+			numCount := numCount + 1;
+		end loop;
+	end if;
+end
+$$ language plpgsql;
+
+
+
+
+do $$ begin
+	-- Perform executes function and discards the result.
+	perform FibonacciSequence(20);
+end $$;
+
