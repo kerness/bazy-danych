2880d638be3517755afe52b4ef9d06b3633f4e21
Maciej Bąk <maciej.bak260@gmail.com>
2021-05-09 13:03:08 +0200

    procedure

--- cw7/cw7.sql
+++ cw7/cw7.sql
@@ -1,5 +1,7 @@
+-- \qecho
+
 create or replace function FibonacciSequence(upTo int) returns void as $$
-declare 
+declare
 	num1 integer := 0;
 	num2 integer := 1;
 	numCount integer := 0;
@@ -23,11 +25,16 @@ begin
 end
 $$ language plpgsql;
 
+-- do $$ begin
+-- 	-- Perform executes function and discards the result.
+-- 	perform FibonacciSequence(20);
+-- end $$;
 
+create procedure WriteFibonacci(upTo int)
+language SQL
+	as $$
+		-- Perform executes function and discards the result.
+		perform FibonacciSequence(20);
+	$$;
 
-
-do $$ begin
-	-- Perform executes function and discards the result.
-	perform FibonacciSequence(20);
-end $$;
-
+CALL WriteFibonacci(20);
